fdb404eb28f00e6d83a2205e9e0dfdd5de4e4ab6
Cristi Neames <crisneames@gmail.com>
2020-01-06 09:20:39 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+//represents 1st card user flips
+var cardOne = cards[0];
+
+cardsInPlay.push('cardOne');
+
+console.log("User flipped queen");
+//represents 2nd card user flips
+var cardTwo = cards[2];
+//add cardTwo to player hand
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+		alert("You've found a match!")
+	}
+		else {
+			alert("Sorry, try again.")
+		}
+}
+
+
